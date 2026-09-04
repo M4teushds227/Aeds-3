f20b50bad2ba734f6f21a5cbcb7f778aeb7cd341
MateusBerry <mateuscpires57@gmail.com>
2026-09-04 15:52:14 -0300

    Concerto da escrita Binaria

--- Src/Principal.java
+++ Src/Principal.java
@@ -1,54 +1,21 @@
-import java.io.File;
-import java.io.RandomAccessFile;
-import java.nio.charset.StandardCharsets;
+//import java.io.File;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.util.Scanner;
 //import java.lang.NumberFormatException;
 public class Principal{
     public static void main(String[] args)throws Exception{
-        File arquivo = new File("Dados.bin");
-        if(!arquivo.exists()){
-            arquivo.createNewFile();
-        }
-        RandomAccessFile raf = new RandomAccessFile(arquivo, "rw");
-        String algo = "Algo";
-        byte[] binario = algo.getBytes(StandardCharsets.UTF_8);
-        //String resul = raf.readLine();
-        /*if(resul != null && resul.compareTo(algo) == 0){
-            System.out.println("Acesso negado");
-        }*/
-        //else{
-            //raf.writeBytes(algo);
-            //raf.writeBytes(System.lineSeparator());
-            raf.writeBytes(Integer.toBinaryString(binario[0] & 0xFF) /*+ " "*/);
-            raf.writeBytes(System.lineSeparator());
-            raf.writeBytes(Integer.toBinaryString(binario[1] & 0xFF) /*+ " "*/);
-            raf.writeBytes(System.lineSeparator());
-            raf.writeBytes(Integer.toBinaryString(binario[2] & 0xFF) /*+ " "*/);
-            raf.writeBytes(System.lineSeparator());
-            raf.writeBytes(Integer.toBinaryString(binario[3] & 0xFF) /*+ " "*/);
-            raf.writeBytes(System.lineSeparator());
-
-            raf.seek(0);
-            System.out.print((char)Integer.parseInt(raf.readLine(), 2) /*+ " "*/);
-            System.out.print((char)Integer.parseInt(raf.readLine(), 2) /*+ " "*/);
-            System.out.print((char)Integer.parseInt(raf.readLine(), 2) /*+ " "*/);
-            System.out.println((char)Integer.parseInt(raf.readLine(), 2) /*+ " "*/);
-            /*raf.seek(1);
-            System.out.print(raf.read() + " ");
-            raf.seek(2);
-            System.out.print(raf.read() + " ");
-            raf.seek(3);
-            System.out.print(raf.read() + " ");
-            raf.seek(4);
-            System.out.print(raf.read() + " ");
-            raf.seek(5);
-            System.out.print(raf.read() + " ");
-            raf.seek(6);
-            System.out.print(raf.read() + " ");*/
-            /*raf.seek(7);
-            System.out.println(raf.read() + " ");*/
-            //}
-        raf.close();
+        FileOutputStream arquivo = new FileOutputStream("Dados.db");
+        DataOutputStream escritor = new DataOutputStream(arquivo);
+        int dado = 10;
+        escritor.writeInt(dado);
+        escritor.writeBytes("Algo existente");
+        //RandomAccessFile raf = new RandomAccessFile(arquivo, "rw");
+        //String algo = "Algo";
+        //byte[] binario = algo.getBytes(StandardCharsets.UTF_8);
+        //raf.close();
+        escritor.close();
+        arquivo.close();
         Scanner scan = new Scanner(System.in);
         scan.close();
     }
